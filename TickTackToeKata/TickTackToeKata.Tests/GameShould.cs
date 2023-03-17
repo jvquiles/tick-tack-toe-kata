2d8f444935a7a9419cd68347417a63a13766cbda
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-17 15:36:03 +0100

    test: Create print empty board test

---
+++ TickTackToeKata/TickTackToeKata.Tests/GameShould.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace TickTackToeKata.Tests;
+
+[TestFixture]
+public class GameShould
+{
+    private Game _game;
+
+    [SetUp]
+    public void Setup()
+    {
+        _game = new Game(3, 3);
+    }
+    
+    [Test]
+    public void PrintEmptyBoard()
+    {
+        var result = _game.PrintBoard();
+        Assert.That(result, Is.EqualTo("[ ][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]\n"));
+    }
+}
+
+public class Game
+{
+    public Game(int i, int i1)
+    {
+    }
+
+    public string PrintBoard()
+    {
+        return string.Empty;
+    }
+}
