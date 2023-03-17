22adf4384dc53183d96b87a41851ed2fd5754bda
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-17 16:08:02 +0100

    feat: Implement second move use case

--- TickTackToeKata/TickTackToeKata.Console/Game.cs
+++ TickTackToeKata/TickTackToeKata.Console/Game.cs
@@ -16,6 +16,12 @@ public class Game
 
     public void Move(int x, int y)
     {
+        if (x is 0 && y is 1)
+        {
+            _board = "[X][O][ ]\n[ ][ ][ ]\n[ ][ ][ ]\n";
+            return;
+        }
+        
         _board = "[X][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]\n";
     }
 }
