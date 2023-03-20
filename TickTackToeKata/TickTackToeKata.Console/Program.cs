60660db250ebf482c8f1e3dd05894f41b12dd0e2
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-20 12:32:08 +0100

    feat: Implement program request for coordinates and updating the board

--- TickTackToeKata/TickTackToeKata.Console/Program.cs
+++ TickTackToeKata/TickTackToeKata.Console/Program.cs
@@ -4,3 +4,14 @@ using TickTackToeKata.Console;
 
 var game = new Game();
 Console.WriteLine(game.PrintBoard());
+
+while (true)
+{
+    Console.Write("Enter X coordinate: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter Y coordinate: ");
+    int y = Convert.ToInt32(Console.ReadLine());
+    game.Move(x, y);
+    Console.Clear();
+    Console.WriteLine(game.PrintBoard());
+}
