3ca8ba1ffbefedb1aa277dfa3750151e1ead247e
Juanvi Quiles <juquiles@aidacanarias.com>
2023-03-20 11:35:16 +0100

    refact: Rework Move method

--- TickTackToeKata/TickTackToeKata.Console/Game.cs
+++ TickTackToeKata/TickTackToeKata.Console/Game.cs
@@ -18,20 +18,7 @@ public class Game
 
     public void Move(int x, int y)
     {
-        if (x is 0 && y is 1)
-        {
-            _board.Move("[X][O][ ]\n[ ][ ][ ]\n[ ][ ][ ]\n", 0, 0, _currentToken);
-            return;
-        }
-
-        if (x is 1 && y is 1)
-        {
-            _board.Move("[X][O][ ]\n[ ][X][ ]\n[ ][ ][ ]\n", 0, 0, _currentToken);
-            return;
-        }
-
-        _board.Move("[X][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]\n", 0, 0, _currentToken);
-
+        _board.Move("[X][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]\n", x, y, _currentToken);
         _currentToken = _currentToken == "X" ? "O" : "X";
     }
 }
